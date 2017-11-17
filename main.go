26a3f8b94f29273e134a917588f3946211625bb3
Siva <siva@Admins-MacBook-Pro.local>
2017-11-17 11:10:40 +0530

    Fixed the readline issue

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ import (
 func main() {
 
 	var recordsCount int64
-	file, err := os.Open("/Users/siva/Downloads/siva.txt")
+	file, err := os.Open("/home/siva/LatestAppOpenUsers_20170512_to_20171107.txt")
 	defer file.Close()
 
 	if err != nil {
